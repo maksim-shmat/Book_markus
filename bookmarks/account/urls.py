bba54b1ef4d97577a9474d3a664f873ce6ca3412
maksim-shmat <maksim.shmat@gmail.com>
2020-10-30 12:16:01 +0300

    Ready to merge

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,6 +1,24 @@
 from django .urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-        path('login/', views.user_login, name='login'),
+        #path('login/', views.user_login, name='login'),
+        path('login/', auth_views.LoginView.as_view(), name='login'),
+
+        path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+        path('', views.dashboard, name='dashboard'),
+
+        path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+
+        path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+        path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+
+        path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+
+        path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+        path('register/', views.register, name='register')
 ]
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bookmarks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/home/django2/Kalilaska-shop/bookmarks/account/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECTED_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
