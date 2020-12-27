c019f514b883fc9c720e6f2ec096fa049456e48a
maksim-shmat <62420547+maksim-shmat@users.noreply.github.com>
2020-12-27 19:19:00 +0300

    Update settings.py

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '43_q#l046ih3=5s3heb_uc(=o40-_dl+=g$6%60roja74poqv('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
